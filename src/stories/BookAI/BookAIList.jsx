8c7da1196ffcec3859ae7dda23bcbca6005deac6
Hicham <hic.mait@gmail.com>
2026-07-21 10:29:02 +0100

    book

--- src/stories/BookAI/BookAIList.jsx
+++ src/stories/BookAI/BookAIList.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 // import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { MemoryRouter } from "react-router-dom";
+// import { MemoryRouter } from "react-router-dom";
 
-import { BookAIListComponent } from "./BookAIListComponent";
+// import { BookAIListComponent } from "./BookAIListComponent";
+
+// Create a client instance
+// const queryClient = new QueryClient();
 
 export const BookAIList = (props) => {
-  return (
-    <MemoryRouter initialEntries={["/"]}>
-      <BookAIListComponent {...props} />
-    </MemoryRouter>
-  );
+  return <h1>kjlkjlk</h1>;
 };
